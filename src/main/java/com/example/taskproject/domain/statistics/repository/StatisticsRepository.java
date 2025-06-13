b5cf03c11db34b07198b54d562e7db70dc269b65
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-13 20:19:18 +0900

    remove: statisticsRepository file remove

--- src/main/java/com/example/taskproject/domain/statistics/repository/StatisticsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskproject.domain.statistics.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatisticsRepository extends JpaRepository {
-}
-
