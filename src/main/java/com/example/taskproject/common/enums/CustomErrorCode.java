163ba57597f09e3245a4701b3ae62337979bc2f2
LeeHyeonha <lej5769@gmail.com>
2025-06-16 19:31:08 +0900

    Refactor: 에러 코드 수정

--- src/main/java/com/example/taskproject/common/enums/CustomErrorCode.java
+++ src/main/java/com/example/taskproject/common/enums/CustomErrorCode.java
@@ -16,12 +16,10 @@ public enum CustomErrorCode {
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED,"잘못된 사용자명 또는 비밀번호입니다"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않거나 탈퇴한 유저입니다."),
     PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-    ;
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
-    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다."),
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
+    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "일정이 존재하지 않습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."),
     COMMENT_NOT_ENTERED(HttpStatus.BAD_REQUEST, "댓글이 작성되지 않았습니다."),
-    COMMENT_IS_EQUAL(HttpStatus.BAD_REQUEST, "수정된 댓글이 기존 댓글과 동일합니다.");
+    COMMENT_IS_EQUAL(HttpStatus.BAD_REQUEST, "수정된 댓글이 기존 댓글과 동일합니다."),;
 
     private final HttpStatus httpStatus;
     private final String message;
