14e2672e96ad70d4cb76f4272fcff8c0218fe8b2
easter1201 <kite1201kr@gmail.com>
2025-06-16 19:53:02 +0900

    feat: 로그 생성 기능 추가

--- src/main/java/com/example/taskproject/common/entity/ActiveLog.java
+++ src/main/java/com/example/taskproject/common/entity/ActiveLog.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Entity
 @Table(name = "active_logs")
 @EntityListeners(AuditingEntityListener.class)
--- src/main/java/com/example/taskproject/domain/activelog/service/ActiveLogService.java
+++ src/main/java/com/example/taskproject/domain/activelog/service/ActiveLogService.java
@@ -3,6 +3,7 @@ package com.example.taskproject.domain.activelog.service;
 import com.example.taskproject.common.dto.LogRequestDto;
 import com.example.taskproject.common.entity.ActiveLog;
 import com.example.taskproject.domain.activelog.repository.ActiveLogRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,16 @@ public class ActiveLogService {
 
     public Page<ActiveLog> getActiveLogsByConditions(LogRequestDto logRequest, Pageable pageable){
         return activeLogRepository.findActiveLogsDynamic(logRequest, pageable);
+    }
+
+    public void logActivity(Long userId, String activityType, Long targetId, HttpServletRequest request){
+        ActiveLog log = new ActiveLog();
+        log.setUserId(userId);
+        log.setActivityType(activityType);
+        log.setTargetId(targetId);
+        log.setIp(request.getRemoteAddr());
+        log.setRequestMethod(request.getMethod());
+        log.setRequestUrl(request.getRequestURI());
+        activeLogRepository.save(log);
     }
 }
