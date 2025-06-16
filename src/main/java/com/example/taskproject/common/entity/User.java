2fdbe48c7195652289c9167ac4eb9add4423a990
Geyapse <8290m@naver.com>
2025-06-16 15:17:46 +0900

    feat: TaskService 단건조회,전체조회 기능 테스트 코드 추가

--- src/main/java/com/example/taskproject/common/entity/User.java
+++ src/main/java/com/example/taskproject/common/entity/User.java
@@ -2,9 +2,10 @@ package com.example.taskproject.common.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Setter
 @Getter
 @Entity
 @Table(name = "users")
