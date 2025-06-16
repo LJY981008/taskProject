63cdb103ee13cf00b7d5360837ec987668fe0270
kodongwon <dw9706@gmail.com>
2025-06-16 16:30:17 +0900

    Feat: SecurityConfig 접근 권한 설정
    - 회원가입, 로그인 URI 제외 모든 경로 authenticated

--- src/main/java/com/example/taskproject/common/config/SecurityConfig.java
+++ src/main/java/com/example/taskproject/common/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/", "/swagger-ui.html", "/v3/api-docs/").permitAll()
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/auth/register","api/auth/login").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(configure -> configure
