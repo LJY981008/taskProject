96067adea813bd36d33bc07fc0834f716f078241
easter1201 <kite1201kr@gmail.com>
2025-06-18 17:42:55 +0900

    refactor: ActiveLogController 코드 간결화

--- src/main/java/com/example/taskproject/domain/activelog/controller/ActiveLogController.java
+++ src/main/java/com/example/taskproject/domain/activelog/controller/ActiveLogController.java
@@ -4,15 +4,11 @@ import com.example.taskproject.common.dto.LogRequestDto;
 import com.example.taskproject.common.entity.ActiveLog;
 import com.example.taskproject.common.util.CustomMapper;
 import com.example.taskproject.domain.activelog.service.ActiveLogService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.Map;
 
 @RestController
@@ -22,32 +18,9 @@ public class ActiveLogController {
     private final ActiveLogService activeLogService;
 
     @GetMapping("/my")
-    public ResponseEntity<Map<String, Object>> findLogsByConditions(
-            @RequestParam(required = false) Long userId,
-            @RequestParam(required = false) String activityType,
-            @RequestParam(required = false) Long targetId,
-            @RequestParam(required = false) LocalDate startDate,
-            @RequestParam(required = false) LocalDate endDate,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "true") boolean sortByTime
-    ){
-        LogRequestDto logRequest = new LogRequestDto(
-                userId,
-                activityType,
-                targetId,
-                startDate,
-                endDate,
-                page,
-                size,
-                sortByTime
-        );
+    public ResponseEntity<Map<String, Object>> findLogsByConditions(@ModelAttribute LogRequestDto logRequest){
+        Page<ActiveLog> logPage = activeLogService.getActiveLogsByConditions(logRequest, logRequest.toPageable());
 
-        Pageable pageable = logRequest.toPageable();
-        Page<ActiveLog> logPage = activeLogService.getActiveLogsByConditions(logRequest, pageable);
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(CustomMapper.responseToMap(logPage, true));
+        return ResponseEntity.ok(CustomMapper.responseToMap(logPage, true));
     }
-
 }
