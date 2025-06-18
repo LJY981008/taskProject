63c4edd33f66a62266851ea906b4217b64e44cde
kodongwon <dw9706@gmail.com>
2025-06-18 19:12:20 +0900

    refactor: TaskService 리팩토링

--- src/main/java/com/example/taskproject/common/entity/Task.java
+++ src/main/java/com/example/taskproject/common/entity/Task.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -24,9 +25,11 @@ public class Task extends BaseEntity {
     private String contents;
 
     @Column(name = "priority")
+    @Enumerated(value = EnumType.STRING)
     private TaskPriority taskPriority;
 
     @Column(name = "status")
+    @Enumerated(value = EnumType.STRING)
     private TaskStatus taskStatus;
 
     @Column(name = "dueDate")
@@ -49,5 +52,13 @@ public class Task extends BaseEntity {
         this.taskId = taskId;
         this.title = title;
         this.author = author;
+    }
+
+    public void update(String title, String contents, TaskPriority taskPriority,LocalDateTime dueDate, User manager){
+        Optional.ofNullable(title).ifPresent(t -> this.title = t);
+        Optional.ofNullable(contents).ifPresent(c -> this.contents = c);
+        Optional.ofNullable(taskPriority).ifPresent(tp -> this.taskPriority = tp);
+        Optional.ofNullable(dueDate).ifPresent(d -> this.dueDate = d);
+        Optional.ofNullable(manager).ifPresent(m -> this.manager = m);
     }
 }
