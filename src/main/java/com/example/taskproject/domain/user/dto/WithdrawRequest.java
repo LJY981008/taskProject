034afc9df0c5f081bdc3eeb7db7456e2f4f64111
kodongwon <dw9706@gmail.com>
2025-06-18 16:51:27 +0900

    refactor: 유저 DTO Bean Validation 메시지 추가

--- src/main/java/com/example/taskproject/domain/user/dto/WithdrawRequest.java
+++ src/main/java/com/example/taskproject/domain/user/dto/WithdrawRequest.java
@@ -1,5 +1,6 @@
 package com.example.taskproject.domain.user.dto;
 
+import com.example.taskproject.common.constant.ValidationMessage;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class WithdrawRequest {
-    @NotBlank
+    @NotBlank(message = ValidationMessage.PASSWORD_REQUIRED)
     private String password;
 
     public WithdrawRequest(String password) {
